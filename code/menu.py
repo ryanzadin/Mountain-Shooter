3323ecf8947a6f7d2e7869e2c0423f35fbad47e5
Ryan moraes <ryandulko458@gmail.com>
2026-05-27 12:12:59 -0300

    Create main loop connect classes

---
+++ code/menu.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.Attribute1 = None
+
+    def run(self,):
+        pass
